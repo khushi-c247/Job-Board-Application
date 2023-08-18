4bb6fb1cd14ade4df0f1f3465562145f259877a5
khushi-c247 <khushi.c@chapter247.com>
2023-08-18 19:34:14 +0530

    Express-Validator

--- src/Router/userRouter.ts
+++ src/Router/userRouter.ts
@@ -1,17 +1,20 @@
 import express from 'express'
 import { jobListing, newApplication, newUsercrete, findJob ,getUserController , loginController} from '../Controller/userController'
-import { query , validationResult , body } from 'express-validator'
+import validateMiddleware from '../Middleware/validators'
+import authorization from "../Middleware/auth"
+import {normal} from "../helper/constants"
 import passport from '../config/passport'
 const router = express.Router()
 
-router.get('/getUser', passport.authenticate('jwt', {session : false}) , getUserController)
-router.post('/login' , body("email").notEmpty(), body("password").notEmpty(),loginController)
+//To-do --> move this API to Admin
+router.get('/getUser', passport.authenticate('jwt', {session : false}), authorization(normal), getUserController)
+router.post('/login' , validateMiddleware, loginController)
 
 //User routers
-router.post("/job-application",passport.authenticate('jwt', {session : false}), newApplication)
+router.post("/job-application",passport.authenticate('jwt', {session : false}), authorization(normal), newApplication)
 router.post("/new-user", newUsercrete)
 
-//serching routers
+//serching routers 
 router.post("/find-job/:id", findJob)
 router.get("/job-openings", jobListing)
 
